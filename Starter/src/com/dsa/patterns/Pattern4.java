f2448a0055c9b6d91c34238795a7045332e306c3
Deepak <deepak.281295@gmail.com>
2023-12-03 15:00:32 +0530

    Pattern4

---
+++ Starter/src/com/dsa/patterns/Pattern4.java
@@ -0,0 +1,22 @@
+package com.dsa.patterns;
+
+import java.util.Scanner;
+
+public class Pattern4 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i <= j) {
+                    System.out.print("*\t");
+                }
+                else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
