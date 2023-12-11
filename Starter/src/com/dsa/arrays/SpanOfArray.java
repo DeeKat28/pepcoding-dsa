4dd95d343f763de433bc31ea7f75850c30c88d4e
Deepak <deepak.281295@gmail.com>
2023-12-11 15:41:51 +0530

    Span of Array

---
+++ Starter/src/com/dsa/arrays/SpanOfArray.java
@@ -0,0 +1,24 @@
+package com.dsa.arrays;
+
+import java.util.Scanner;
+
+public class SpanOfArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            min = Math.min(min, arr[i]);
+            max = Math.max(max, arr[i]);
+        }
+        System.out.println(max - min);
+    }
+}
