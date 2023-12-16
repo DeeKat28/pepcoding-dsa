19f7d4c6a1633668becf11eb5e6c03c4af8fd30d
Deepak <deepak.281295@gmail.com>
2023-12-16 17:46:20 +0530

    Rotate By 90Deg

---
+++ Starter/src/com/dsa/arrays_2d/RotateBy90Deg.java
@@ -0,0 +1,53 @@
+package com.dsa.arrays_2d;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class RotateBy90Deg {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(br.readLine());
+            }
+        }
+
+        rotate(arr);
+        display(arr);
+    }
+
+    public static void rotate(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr[0].length; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            reverseArray(arr[i]);
+        }
+    }
+
+    public static void reverseArray(int[] a) {
+        for (int i = 0, j = a.length - 1; i < j; i++, j--) {
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+
+    public static void display(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
